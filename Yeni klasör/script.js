1e5f06cfec80cf5081f796232059e20ff6e658f6
Anıl Ateş <anil.ates3458@gmail.com>
2025-01-06 12:29:54 +0300

    first commit

---
+++ Yeni klasör/script.js
@@ -0,0 +1,14 @@
+function login() {
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+   
+    if (email === 'abc@gmail.com' && password === '1234') {
+
+        window.location.href = 'index.html';
+    } else {
+             alert('Invalid email or password!');
+    }
+}
+
